4bc198d19f2421a89314190ab91787f44f1c6f1f
Faris-AbdoAlkhaliq <fr1423fr@gmail.com>
2025-11-18 18:24:47 +0300

    major update on CSS stying (inline, internal, external) v1.3

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("EyeTune project loaded successfully.")
+console.log("EyeTune project loaded successfully.");
+
+function changeMessage() {
+  let msg = document.getElementById("dynamicText");
+  msg.innerText = "EyeTune recommends taking a break now!";
+  msg.style.color = "blue";
+}
+
+function darkModeToggle() {
+  let body = document.body;
+  body.style.background = "#3b2f50ff";
+  body.style.color = "white";
+}
